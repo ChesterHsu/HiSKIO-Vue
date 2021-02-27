555f08be7f1902f1c63db57674b52d37920702f9
徐嘉輝 <aa0909286667@gmail.com>
2021-02-27 18:59:56 +0800

    新增 nuxt Tailwind CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    ['@nuxtjs/tailwindcss']
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
